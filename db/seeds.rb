67ba685b615bff30e2390b0d8bec13f840a823e2
Linda <ltramos7@gmail.com>
2020-08-17 18:19:37 -0500

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Artist.destroy_all
+Mural.destroy_all
+Favorite.destroy_all
+
+user_one = User.create(first_name: "Mick", last_name: "Roy", username: "mroy", password: "password123")
+
+user_two = User.create(first_name: "Sandra", last_name: "Collette", username: "scollette", password: "pword123")
+
+artist_one = Artist.create(first_name: "Marry", last_name: "Poppins", bio: "Supercalifragilisticexpealidotious")
+
+artist_two = Artist.create(first_name: "Steve", last_name: "McQueen", bio: "I don't know.")
+
+mural_one = Mural.create(artist_id: artist_one.id, mural_title: "some title", year_installed: "2020", street_address: "123 Main Stree", zip: "60622", description: "This is a desription.")
+
+mural_two = Mural.create(artist_id: artist_two.id, mural_title: "Crazy Stuff!", year_installed: "1919", street_address: "321 Secret Street", zip: "22606", description: "It's cool I guess.")
+
+favorite_one = Favorite.create(user_id: user_two.id, mural_id: mural_one.id)
+
+favorite_two = Favorite.create(user_id: user_one.id, mural_id: mural_two.id)
+
+
+
+
+
+
