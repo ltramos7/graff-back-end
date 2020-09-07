f583b831d26054a7501a9284b24170cbe316931e
Linda <ltramos7@gmail.com>
2020-09-07 12:20:48 -0500

    created a test form to sign up new users. Added rack-cors gem, used old Graffito project to fix rack-cors issue in application rb file.  Next step will be to incorporate Redux and refactor the sign up form.

--- Gemfile
+++ Gemfile
@@ -54,3 +54,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'active_model_serializers'
+
+gem 'rack-cors', '~> 0.4.0'
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :username, :password
-  has_many :favorites
+  # attributes :id, :first_name, :last_name, :username, :password
+  attributes :id, :first_name, :last_name, :username
+  # has_many :favorites
 end
